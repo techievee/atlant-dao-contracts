e6f63c44aac8c01f4fd127a70bae328c5a5f186a
DESKTOP-D9ELAA2\Paul <pashafromrussia63@gmail.com>
2017-10-05 21:20:44 +0300

    remove leftovers

--- contracts/ATL.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.11;
-
-import "./installed/token/StandardToken.sol";
-
-contract ATL{
-  mapping (address => uint256) public balanceOf;
-	uint public totalSupply;
-	function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
-}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,3 @@
-var PtoLib = artifacts.require("./PtoLib");
 var Association = artifacts.require("./Association");
 
 var sharesAddress = web3.eth.accounts[0]; //"0x5D80e46379800f17c26D39C5f3f90cA0057CA196";
@@ -10,7 +9,5 @@ var lawyerFee = 10;
 var lawyerAddress = web3.eth.accounts[1];//"0x9c8E4537517cCac0e2fd3B2f3cD976EBC94F3547";
 
 module.exports = function(deployer) {
-  deployer.deploy(PtoLib);
-  deployer.link(PtoLib, Association);
   deployer.deploy(Association, sharesAddress, minimumSharesToPassAVote, minutesForDebate, defaultPercentFee, lawyerName, lawyerFee, lawyerAddress);
 };
